60649db9ca14a87f12b5011d5eddc20ea92355a9
and <surftware@gmail.com>
2024-05-10 13:57:28 -0700

    avance peticion a una API}

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <div class="col-md-6 offset-md-3">
           <form role="form-control formulario" method="POST" action="index.php">
 
-            <h3>Programa que calcula el factorial</h3>
+            <h3>Número que calcula el factorial</h3>
             <input class="form-control" type="text" name="consultar">
             <input class="btn-buscar btn-default " type="submit" name="enviar" VALUE="Calcula Factorial">
           </form>
